679f16164f4ed71b1daf5a9e9eebc92111c19c5e
Rohit Sahu <rohitraj.r@hotmail.com>
2024-10-11 19:54:37 +0530

    Recursive Approach

--- src/main/java/io/devzona/interview_practices/problem_solving/easy/FindLargestElementInArray.java
+++ src/main/java/io/devzona/interview_practices/problem_solving/easy/FindLargestElementInArray.java
@@ -37,4 +37,29 @@ public class FindLargestElementInArray {
         Arrays.sort(arr);
         return arr[arr.length - 1];
     }
+
+
+    /**
+     * Solution2: Using a max variable (Recursive Approach)
+     * Intuition:
+     * We can maintain a max variable that will update whenever the current value is greater than the value in the max variable.
+     *
+     * Approach:
+     * Create a max variable and initialize it with arr[0].
+     * Use a for loop and compare it with other elements of the array
+     * If any element is greater than the max value, update max value with the element’s value
+     * Print the max variable.
+     *
+     * @param arr
+     * @return largest number
+     */
+    public static int findLargestElement(int[] arr) {
+        int max= arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max= arr[i];
+            }
+        }
+        return max;
+    }
 }
