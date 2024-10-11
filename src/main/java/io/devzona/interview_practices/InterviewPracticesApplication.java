0c6802fc1ade9769925a1f7ac2e6ef7ebf62ffed
Rohit Sahu <rohitraj.r@hotmail.com>
2024-10-11 17:54:11 +0530

    Initial Commit

---
+++ src/main/java/io/devzona/interview_practices/InterviewPracticesApplication.java
@@ -0,0 +1,13 @@
+package io.devzona.interview_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devzona/interview_practices/InterviewPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devzona.interview_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
