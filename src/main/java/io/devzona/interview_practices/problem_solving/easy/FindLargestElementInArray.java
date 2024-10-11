a0757d12fd14d136c5fa67f11cf7e797ea1d0f87
Rohit Sahu <rohitraj.r@hotmail.com>
2024-10-11 19:16:03 +0530

    Added Docs

--- src/main/java/io/devzona/interview_practices/problem_solving/easy/FindLargestElementInArray.java
+++ src/main/java/io/devzona/interview_practices/problem_solving/easy/FindLargestElementInArray.java
@@ -5,6 +5,22 @@ import java.util.Arrays;
 /**
  * Given an array, we have to find the largest element in the array.
  *
+ * Example 1:
+ * Input:
+ *  arr[] = {2,5,1,3,0};
+ * Output:
+ *  5
+ * Explanation:
+ *  5 is the largest element in the array.
+ *
+ * Example2:
+ * Input:
+ *  arr[] = {8,10,5,7,9};
+ * Output:
+ *  10
+ * Explanation:
+ *  10 is the largest element in the array.
+ *
  */
 public class FindLargestElementInArray {
 
